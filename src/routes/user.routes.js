82da35d63894c5b49145d9d56a8c667745dbc371
yash kikani <yashkikani@yashs-MacBook-Air.local>
2024-03-21 13:17:04 +0530

    Add remained models

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -24,7 +24,7 @@ router.route("/login").post(loginUser)
 router.route("/logout").post( verifyJWT , logoutUser )
 router.route("/refreshToken").post( refreshAccessToken )
 router.route("/changePassword").post( verifyJWT , changeCurrentPassword )
-router.route("/currentUser").post( verifyJWT , getCurrentUser )
+router.route("/currentUser").get( verifyJWT , getCurrentUser )
 router.route("/updateAccount").patch( verifyJWT , updateAccountDetails )
 
 router.route("/avatar").patch( verifyJWT , upload.single("avatar") , updateUserAvatar )
