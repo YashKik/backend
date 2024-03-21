ae826d7d6d5249c44df39218aa6e23aa4823fb41
yash kikani <yashkikani@yashs-MacBook-Air.local>
2024-03-21 11:48:25 +0530

    add new routes for user controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerUser, loginUser, logoutUser, refreshAccessToken } from "../controllers/user.controllers.js";
+import { registerUser, loginUser, logoutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage, getUserChannelProfile, getWatchHistory } from "../controllers/user.controllers.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -22,6 +22,16 @@ router.route("/login").post(loginUser)
 
 // Secured routes
 router.route("/logout").post( verifyJWT , logoutUser )
-router.route("/refreshToken").post(refreshAccessToken)
+router.route("/refreshToken").post( refreshAccessToken )
+router.route("/changePassword").post( verifyJWT , changeCurrentPassword )
+router.route("/currentUser").post( verifyJWT , getCurrentUser )
+router.route("/updateAccount").patch( verifyJWT , updateAccountDetails )
+
+router.route("/avatar").patch( verifyJWT , upload.single("avatar") , updateUserAvatar )
+router.route("/coverImage").patch( verifyJWT , upload.single("coverImage") , updateUserCoverImage )
+
+router.route("/c/:username").get( verifyJWT , getUserChannelProfile ) // : means req.params when we used
+router.route("/history").get( verifyJWT , getWatchHistory )
+
 
 export default router;
